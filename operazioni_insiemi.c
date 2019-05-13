176d867bc8dd94e27b177fe03c4e70b48c4f7445
Ale Serafini <aleserafini@MacBook-Pro-di-Ale.local>
2019-05-13 22:07:52 +0200

    library comments

--- operazioni_insiemi.c
+++ operazioni_insiemi.c
@@ -1,53 +1,116 @@
+/**************************************************************************/
+/* File di implementazione della libreria gestisce insiemi di numeri reali
+ * esportando le seguenti quattro funzioni. La prima funzione ha come
+ * parametri di ingresso due insiemi e stabilisce ricorsivamente se
+ * sono uguali. La seconda funzione ha come  parametri di ingresso due
+ * insiemi e restituisce la loro intersezione calcolata ricorsivamente.
+ * La terza funzione ha come parametri di ingresso due insiemi e restituisce
+ * la loro differenza calcolata ricorsivamente. La quarta funzione ha come
+ * parametri di ingresso due insiemi e restituisce la loro differenza
+ * simmetrica calcolata ricorsivamente.  */
+/**************************************************************************/
+
+/*****************************/
+/* inclusione delle librerie */
+/*****************************/
+
 #include "operazioni_insiemi.h"
 
-bool verifica_uguaglianza(el_insieme_t *el_primo_insieme,
-                          el_insieme_t *el_secondo_insieme);
+/******************************************/
+/* Dichiarazione delle funzioni esportate */
+/******************************************/
 
-el_insieme_t *calcola_intersezione(el_insieme_t *el_primo_insieme,
-                                   el_insieme_t *el_secondo_insieme);
+/* Dichiarazione della funzione che restituisce l'intersezione
+ * fra due insiemi calcolata ricorsivamente. */
+bool verifica_uguaglianza(el_insieme_t *el_primo_insieme,    /* Elemento testa
+                                                              * del primo insieme */
+                          el_insieme_t *el_secondo_insieme); /* Elemento testa
+                                                              * del secondo insieme */
 
-el_insieme_t *calcola_differenza(el_insieme_t *el_primo_insieme,
-                                 el_insieme_t *el_secondo_insieme);
+/* Dichiarazione della funzione che restituisce l'intersezione
+* fra due insiemi calcolata ricorsivamente. */
+el_insieme_t *calcola_intersezione(el_insieme_t *el_primo_insieme,    /* Elemento testa
+                                                                       * del primo insieme */
+                                   el_insieme_t *el_secondo_insieme); /* Elemento testa
+                                                                       * del secondo insieme */
 
-el_insieme_t *calcola_diff_simm(el_insieme_t *el_primo_insieme,
-                                el_insieme_t *el_secondo_insieme);
+/* Dichiarazione della funzione che restituisce la differenza
+* di due insiemi calcolata ricorsivamente. */
+el_insieme_t *calcola_differenza(el_insieme_t *el_primo_insieme,    /* Elemento testa
+                                                                     * del primo insieme */
+                                 el_insieme_t *el_secondo_insieme); /* Elemento testa
+                                                                     * del secondo insieme */
 
+/* Dichiarazione della funzione che restituisce la differenza
+* simmetrica di due insiemi calcolata ricorsivamente. */
+el_insieme_t *calcola_diff_simm(el_insieme_t *el_primo_insieme,    /* Elemento testa
+                                                                    * del primo insieme */
+                                el_insieme_t *el_secondo_insieme); /* Elemento testa
+                                                                    * del secondo insieme */
 
-bool verifica_uguaglianza(el_insieme_t *el_primo_insieme,
-                          el_insieme_t *el_secondo_insieme)
+/****************************************/
+/* Definizione delle funzioni esportate */
+/****************************************/
+
+/* Definizione della funzione che restituisce l'intersezione
+ * fra due insiemi calcolata ricorsivamente. */
+bool verifica_uguaglianza(el_insieme_t *el_primo_insieme,   /* Elemento testa
+                                                             * del primo insieme */
+                          el_insieme_t *el_secondo_insieme) /* Elemento testa
+                                                             * del secondo insieme */
 {
-    bool risultato = true;
+    /* Dichiarazione delle variabili locali alla funzione */
+    bool risultato = true; /* Output: valore che indica se i due insiemi sono uguali */
+    
+    /* Verifica che il primo insieme non sia terminato */
     if (el_primo_insieme != NULL)
     {
         if (el_secondo_insieme != NULL)
         {
+            /* Se nemmeno il secondo insieme è terminato,
+             * verifico l'uguaglianza dei due valori in esame */
             if (el_primo_insieme->valore == el_secondo_insieme->valore)
             {
+                /* Nel caso in cui i due valori corrispondano,
+                 * eseguo il controllo per i prossimi due elementi */
                 risultato = verifica_uguaglianza(el_primo_insieme->prossimo,
                                                  el_secondo_insieme->prossimo);
             }
             else
             {
+                /* Nel caso in cui gli elementi siano diversi,
+                 * allora non si ha l'uguaglianza */
                 risultato = false;
             }
         }
         else
         {
+            /* Nel caso in cui il primo insieme non sia terminato
+             * ed il secondo si, allora non si ha l'uguaglianza */
             risultato = false;
         }
     }
     else
     {
+        /* Nel caso in cui il primo insieme sia terminato, l'uguaglianza
+         * si ha solo nel caso in cui lo sia anche il secondo */
         risultato = (el_secondo_insieme == NULL);
     }
     
+    /* Restituzione del risultato dell'uguaglianza */
     return (risultato);
 }
 
-el_insieme_t *calcola_intersezione(el_insieme_t *el_primo_insieme,
-                                   el_insieme_t *el_secondo_insieme)
+/* Definizione della funzione che restituisce l'intersezione
+* fra due insiemi calcolata ricorsivamente. */
+el_insieme_t *calcola_intersezione(el_insieme_t *el_primo_insieme,   /* Elemento testa
+                                                                      * del primo insieme */
+                                   el_insieme_t *el_secondo_insieme) /* Elemento testa
+                                                                      * del secondo insieme */
 {
+    /* Dichiarazione delle variabili locali alla funzione */
     el_insieme_t *nuovo_el = NULL;
+    
     if (el_primo_insieme != NULL &&
         el_secondo_insieme != NULL)
     {
@@ -72,10 +135,16 @@ el_insieme_t *calcola_intersezione(el_insieme_t *el_primo_insieme,
     return (nuovo_el);
 }
 
-el_insieme_t *calcola_differenza(el_insieme_t *el_primo_insieme,
-                                 el_insieme_t *el_secondo_insieme)
+/* Definizione della funzione che restituisce la differenza
+* di due insiemi calcolata ricorsivamente. */
+el_insieme_t *calcola_differenza(el_insieme_t *el_primo_insieme,   /* Elemento testa
+                                                                    * del primo insieme */
+                                 el_insieme_t *el_secondo_insieme) /* Elemento testa
+                                                                    * del secondo insieme */
 {
+    /* Dichiarazione delle variabili locali alla funzione */
     el_insieme_t *nuovo_el = NULL;
+    
     if (el_primo_insieme != NULL)
     {
         if (el_secondo_insieme != NULL)
@@ -109,10 +178,16 @@ el_insieme_t *calcola_differenza(el_insieme_t *el_primo_insieme,
     return (nuovo_el);
 }
 
-el_insieme_t *calcola_diff_simm(el_insieme_t *el_primo_insieme,
-                                el_insieme_t *el_secondo_insieme)
+/* Definizione della funzione che restituisce la differenza
+* simmetrica di due insiemi calcolata ricorsivamente. */
+el_insieme_t *calcola_diff_simm(el_insieme_t *el_primo_insieme,   /* Elemento testa
+                                                                   * del primo insieme */
+                                el_insieme_t *el_secondo_insieme) /* Elemento testa
+                                                                   * del secondo insieme */
 {
+    /* Dichiarazione delle variabili locali alla funzione */
     el_insieme_t *nuovo_el = NULL;
+    
     if (el_primo_insieme != NULL &&
         el_secondo_insieme != NULL)
     {
--- operazioni_insiemi.h
+++ operazioni_insiemi.h
@@ -1,5 +1,27 @@
+/**************************************************************************/
+/* File di intestazione della libreria gestisce insiemi di numeri reali
+ * esportando le seguenti quattro funzioni. La prima funzione ha come
+ * parametri di ingresso due insiemi e stabilisce ricorsivamente se
+ * sono uguali. La seconda funzione ha come  parametri di ingresso due
+ * insiemi e restituisce la loro intersezione calcolata ricorsivamente.
+ * La terza funzione ha come parametri di ingresso due insiemi e restituisce
+ * la loro differenza calcolata ricorsivamente. La quarta funzione ha come
+ * parametri di ingresso due insiemi e restituisce la loro differenza
+ * simmetrica calcolata ricorsivamente.  */
+/**************************************************************************/
+
+/*****************************/
+/* Inclusione delle librerie */
+/*****************************/
+
 #include <stdbool.h>
 #include <stdlib.h>
+
+/************************************/
+/* Ridefinizione dei tipi esportati */
+/************************************/
+
+// TODO: chiedere a marco se questo tipo custom va definito anche all'interno del programma test (funziona anche senza)
 
 typedef struct el_insieme
 {
@@ -7,14 +29,34 @@ typedef struct el_insieme
     struct el_insieme *prossimo;
 } el_insieme_t;
 
-extern bool verifica_uguaglianza(el_insieme_t *el_primo_insieme,
-                                 el_insieme_t *el_secondo_insieme);
+/********************************************/
+/* Ridichiarazione delle funzioni esportate */
+/********************************************/
 
-extern el_insieme_t *calcola_intersezione(el_insieme_t *el_primo_insieme,
-                                          el_insieme_t *el_secondo_insieme);
+/* Ridichiarazione della funzione che stabilisce
+ * ricorsivamente se due insiemi sono uguali. */
+extern bool verifica_uguaglianza(el_insieme_t *el_primo_insieme,    /* Elemento testa
+                                                                     * del primo insieme */
+                                 el_insieme_t *el_secondo_insieme); /* Elemento testa
+                                                                     * del secondo insieme */
 
-extern el_insieme_t *calcola_differenza(el_insieme_t *el_primo_insieme,
-                                        el_insieme_t *el_secondo_insieme);
+/* Ridichiarazione della funzione che restituisce l'intersezione
+ * fra due insiemi calcolata ricorsivamente. */
+extern el_insieme_t *calcola_intersezione(el_insieme_t *el_primo_insieme,    /* Elemento testa
+                                                                              * del primo insieme */
+                                          el_insieme_t *el_secondo_insieme); /* Elemento testa
+                                                                              * del secondo insieme */
 
-extern el_insieme_t *calcola_diff_simm(el_insieme_t *el_primo_insieme,
-                                       el_insieme_t *el_secondo_insieme);
+/* Ridichiarazione della funzione che restituisce la differenza
+ * di due insiemi calcolata ricorsivamente. */
+extern el_insieme_t *calcola_differenza(el_insieme_t *el_primo_insieme,    /* Elemento testa
+                                                                            * del primo insieme */
+                                        el_insieme_t *el_secondo_insieme); /* Elemento testa
+                                                                            * del secondo insieme */
+
+/* Ridichiarazione della funzione che restituisce la differenza
+ * simmetrica di due insiemi calcolata ricorsivamente. */
+extern el_insieme_t *calcola_diff_simm(el_insieme_t *el_primo_insieme,    /* Elemento testa
+                                                                           * del primo insieme */
+                                       el_insieme_t *el_secondo_insieme); /* Elemento testa
+                                                                           * del secondo insieme */
